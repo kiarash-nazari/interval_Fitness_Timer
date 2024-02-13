fccc9ce7e32986e4d7ebf3a707d1cd070bd20213
Kiyarash <naparsis@gmail.com>
2024-02-13 14:48:08 +0200

    slider

--- lib/screens/interval_screen.dart
+++ lib/screens/interval_screen.dart
@@ -21,7 +21,7 @@ class IntervalScreen extends StatelessWidget {
           height: size.height,
           width: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               50.heightBox,
               Row(
@@ -110,13 +110,63 @@ class IntervalScreen extends StatelessWidget {
                 ),
               ),
               CarouselSlider(
-                options: CarouselOptions(),
+                options: CarouselOptions(
+                  height: 185.0,
+                  viewportFraction: 0.6,
+                  initialPage: 0,
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.4,
+                ),
                 items: [
-                  Container(
-                    width: size.width / 2,
-                    color: AppColors.mainblue,
-                    child: Text("data"),
-                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: 200,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        image: DecorationImage(
+                          image: AssetImage("assets/img/ki.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromARGB(127, 188, 186, 186),
+                            blurRadius: 30.0, // soften the shadow
+                            spreadRadius: 5.0, //extend the shadow
+                            offset: Offset(
+                              10.0, // Move to right 10  horizontally
+                              25.0, // Move to bottom 10 Vertically
+                            ),
+                          )
+                        ],
+                      ),
+                      child: Stack(
+                        children: [
+                          CircularPercentIndicator(
+                              linearGradient: const LinearGradient(
+                                  colors: [
+                                    Color(0xFF3366FF),
+                                    Color(0xFF00CCFF),
+                                  ],
+                                  begin: FractionalOffset(0.0, 0.0),
+                                  end: FractionalOffset(1.0, 0.0),
+                                  stops: [0.0, 1.0],
+                                  tileMode: TileMode.clamp),
+                              radius: size.width / 15,
+                              backgroundColor: Colors.grey.shade700,
+                              lineWidth: 5,
+                              circularStrokeCap: CircularStrokeCap.round,
+                              percent: 0.1,
+                              center: const Icon(
+                                Icons.download,
+                                color: Colors.amber,
+                              )),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               )
             ],
